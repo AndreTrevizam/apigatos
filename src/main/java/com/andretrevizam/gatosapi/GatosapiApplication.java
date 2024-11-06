69039331e4172cf697671635737896542144dbad
AndreTrevizam <andretrevizam264@gmail.com>
2024-11-06 15:24:24 -0300

    spring app

---
+++ src/main/java/com/andretrevizam/gatosapi/GatosapiApplication.java
@@ -0,0 +1,13 @@
+package com.andretrevizam.gatosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatosapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andretrevizam/gatosapi/GatosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andretrevizam.gatosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
